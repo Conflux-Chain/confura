c70b5c81b3e5839015855edd331e2c8994e499ea
wanliqun <wanliqunn@163.com>
2021-06-07 11:48:44 +0800

    upgrade go-sdk to v1.0.9 and then master && fix compatability issue (#61)
    to enable rlp encoding/decoding support for block/tx/log etc.,

--- util/cfx.go
+++ util/cfx.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	sdk "github.com/Conflux-Chain/go-conflux-sdk"
+	"github.com/Conflux-Chain/go-conflux-sdk/middleware"
 	"github.com/ethereum/go-ethereum/metrics"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -27,37 +28,47 @@ func MustNewCfxClientWithRetry(url string, retry int, retryInterval time.Duratio
 	cfx, err := sdk.NewClient(url, sdk.ClientOption{
 		RetryCount:    retry,
 		RetryInterval: retryInterval,
-		CallRpcLog:    onCfxSdkRpcCall,
 	})
 
 	if err != nil {
 		logrus.WithError(err).Fatalf("Failed to create CFX client to %v", url)
 	}
 
+	cfx.UseCallRpcMiddleware(callRpcMetricsMiddleware)
+
 	return cfx
 }
 
-func onCfxSdkRpcCall(method string, args []interface{}, result interface{}, resultError error, duration time.Duration) {
-	if resultError != nil {
-		// Update rpc call error counter metrics
-		metricCounter, ok := cfxRpcCallErrorMetricCounters.Load(method)
-		if !ok {
-			metricKey := fmt.Sprintf("infura/duration/cfx/rpc/call/%v/error", method)
-			metricCounter = metrics.GetOrRegisterCounter(metricKey, nil)
-			cfxRpcCallErrorMetricCounters.Store(method, metricCounter)
+func callRpcMetricsMiddleware(handler middleware.CallRpcHandler) middleware.CallRpcHandler {
+	metricFn := func(result interface{}, method string, args ...interface{}) error {
+		start := time.Now()
+		err := handler.Handle(result, method, args...)
+		duration := time.Since(start)
+
+		if err != nil {
+			// Update rpc call error counter metrics
+			metricCounter, ok := cfxRpcCallErrorMetricCounters.Load(method)
+			if !ok {
+				metricKey := fmt.Sprintf("infura/duration/cfx/rpc/call/%v/error", method)
+				metricCounter = metrics.GetOrRegisterCounter(metricKey, nil)
+				cfxRpcCallErrorMetricCounters.Store(method, metricCounter)
+			}
+
+			metricCounter.(metrics.Counter).Inc(1)
+			return err
 		}
 
-		metricCounter.(metrics.Counter).Inc(1)
-		return
+		// Update rpc call success timer metrics
+		metricTimer, ok := cfxRpcCallSuccessMetricTimers.Load(method)
+		if !ok {
+			metricKey := fmt.Sprintf("infura/duration/cfx/rpc/call/%v/success", method)
+			metricTimer = metrics.GetOrRegisterTimer(metricKey, nil)
+			cfxRpcCallSuccessMetricTimers.Store(method, metricTimer)
+		}
+
+		metricTimer.(metrics.Timer).Update(duration)
+		return nil
 	}
 
-	// Update rpc call success timer metrics
-	metricTimer, ok := cfxRpcCallSuccessMetricTimers.Load(method)
-	if !ok {
-		metricKey := fmt.Sprintf("infura/duration/cfx/rpc/call/%v/success", method)
-		metricTimer = metrics.GetOrRegisterTimer(metricKey, nil)
-		cfxRpcCallSuccessMetricTimers.Store(method, metricTimer)
-	}
-
-	metricTimer.(metrics.Timer).Update(duration)
+	return middleware.CallRpcHandlerFunc(metricFn)
 }
