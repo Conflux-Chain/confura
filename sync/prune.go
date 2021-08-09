5f129f44e468f4e712587329343c010cebd45c4c
wanliqun <wanliqunn@163.com>
2021-08-09 15:28:59 +0800

    Refactor pruner with name to make the log more clear (#89)

--- sync/prune.go
+++ sync/prune.go
@@ -22,7 +22,7 @@ type PruneConfig struct {
 	MaxPruneEpochs uint64         `mapstructure:"maxEpochs"` // max epochs to prune if threshold condition met
 }
 
-// Threshold settings for DB pruner
+// Threshold settings for pruner
 type PruneThresHold struct {
 	MaxBlocks uint64 `mapstructure:"maxBlocks"` // max number of blocks to trigger block pruning
 	MaxTxs    uint64 `mapstructure:"maxTxs"`    // max number of transactions to trigger transaction pruning
@@ -36,7 +36,7 @@ func MustNewDBPruner(db store.Store) *Pruner {
 		logrus.WithError(err).Fatal("DBPruner failed to load prune config")
 	}
 
-	return newPruner(db, &pc)
+	return newPruner("DBPruner", db, &pc)
 }
 
 // MustNewKVCachePruner creates an instance of Pruner to prune blockchain data in kv cache
@@ -46,19 +46,20 @@ func MustNewKVCachePruner(cache store.Store) *Pruner {
 		logrus.WithError(err).Fatal("KVPruner failed to load prune config")
 	}
 
-	return newPruner(cache, &pc)
+	return newPruner("KVPruner", cache, &pc)
 }
 
 // Pruner is used to prune blockchain data in store periodly.
 // It will prune blockchain data in store with epoch as the smallest unit to retain data atomicity.
 type Pruner struct {
+	name        string
 	store       store.Store
 	pruneConfig *PruneConfig
 }
 
 // newPruner creates an instance of Pruner to prune blockchain data.
-func newPruner(store store.Store, pc *PruneConfig) *Pruner {
-	return &Pruner{store: store, pruneConfig: pc}
+func newPruner(name string, store store.Store, pc *PruneConfig) *Pruner {
+	return &Pruner{name: name, store: store, pruneConfig: pc}
 }
 
 func (pruner *Pruner) Prune(ctx context.Context, wg *sync.WaitGroup) {
@@ -71,11 +72,11 @@ func (pruner *Pruner) Prune(ctx context.Context, wg *sync.WaitGroup) {
 	for {
 		select {
 		case <-ctx.Done():
-			logrus.Infof("Pruner shutdown ok")
+			logrus.Infof("%v shutdown ok", pruner.name)
 			return
 		case <-ticker.C:
 			if err := pruner.doTicker(); err != nil {
-				logrus.WithError(err).Errorf("Pruner ticked error")
+				logrus.WithError(err).Errorf("%v ticked error", pruner.name)
 			}
 		}
 	}
@@ -84,7 +85,7 @@ func (pruner *Pruner) Prune(ctx context.Context, wg *sync.WaitGroup) {
 func (pruner *Pruner) doTicker() error {
 	for _, dt := range store.OpEpochDataTypes {
 		if err := pruner.pruneEpochData(dt); err != nil {
-			logrus.WithError(err).Errorf("Pruner failed to prune epoch %v", store.EpochDataTypeToStr(dt))
+			logrus.WithError(err).Errorf("%v failed to prune epoch %v", pruner.name, store.EpochDataTypeToStr(dt))
 			return err
 		}
 	}
@@ -115,7 +116,7 @@ func (pruner *Pruner) pruneEpochData(dt store.EpochDataType) error {
 	for numData > threshold {
 		logrus.WithFields(logrus.Fields{
 			"numData": numData, "threshold": threshold,
-		}).Infof("Pruner starting to prune epoch %v", store.EpochDataTypeToStr(dt))
+		}).Infof("%v starting to prune epoch %v", pruner.name, store.EpochDataTypeToStr(dt))
 
 		if err := pruner.doPruneEpochData(dt); err != nil {
 			return err
@@ -156,7 +157,7 @@ func (pruner *Pruner) doPruneEpochData(dt store.EpochDataType) error {
 	epochUntil := minEpoch + pruner.pruneConfig.MaxPruneEpochs - 1
 	epochUntil = util.MinUint64(epochUntil, maxEpoch)
 
-	logrus.WithField("epochUntil", epochUntil).Infof("DB pruner dequeue epoch %v data", store.EpochDataTypeToStr(dt))
+	logrus.WithField("epochUntil", epochUntil).Infof("%v dequeue epoch %v data", pruner.name, store.EpochDataTypeToStr(dt))
 
 	// Dequeue epoch data
 	if err := dequeue(epochUntil); err != nil {
