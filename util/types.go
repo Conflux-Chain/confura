e2e6053470c9937f7770771ba0695a58ebe0112d
wanliqun <wanliqunn@163.com>
2022-02-17 16:46:31 +0800

    Bugfix nil interface check panic on struct value (#220)

--- util/types.go
+++ util/types.go
@@ -5,5 +5,13 @@ import "reflect"
 // Helper function to check if interface value is nil, since "i == nil" checks nil interface case only.
 // Refer to https://mangatmodi.medium.com/go-check-nil-interface-the-right-way-d142776edef1 for more details.
 func IsInterfaceValNil(i interface{}) bool {
-	return i == nil || reflect.ValueOf(i).IsNil()
+	if i == nil {
+		return true
+	}
+
+	if reflect.ValueOf(i).Kind() == reflect.Ptr && reflect.ValueOf(i).IsNil() {
+		return true
+	}
+
+	return false
 }
