1a73f31e09d54090cb633a9c2c0056283bdaada7
Conflux <conflux@ConfluxdeMacBook-Pro.local>
2022-11-02 13:34:49 +0800

    Add eSpace filter API implementation

--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/hashicorp/golang-lru v0.5.5-0.20210104140557-80c98217689d
 	github.com/montanaflynn/stats v0.6.6
 	github.com/openweb3/go-rpc-provider v0.2.9
-	github.com/openweb3/web3go v0.2.0
+	github.com/openweb3/web3go v0.2.2
 	github.com/pkg/errors v0.9.1
 	github.com/royeo/dingrobot v1.0.1-0.20191230075228-c90a788ca8fd
 	github.com/sirupsen/logrus v1.8.1
--- rpc/eth_api_filter.go
+++ rpc/eth_api_filter.go
@@ -3,10 +3,24 @@ package rpc
 import (
 	"context"
 	"errors"
+	"fmt"
+	"strings"
 
-	"github.com/Conflux-Chain/go-conflux-sdk/types"
+	rpcutil "github.com/Conflux-Chain/confura/util/rpc"
+	lru "github.com/hashicorp/golang-lru"
 	"github.com/openweb3/go-rpc-provider"
 	web3Types "github.com/openweb3/web3go/types"
+)
+
+const (
+	defaultFilterQueryCacheSize = 5000
+)
+
+var (
+	// (fullnode name + filter ID) => *web3Types.FilterQuery
+	ethFilterQueryCache, _ = lru.New(defaultFilterQueryCacheSize)
+
+	errFilterNotFound = errors.New("filter not found")
 )
 
 // NewFilter creates a new filter and returns the filter id. It can be
@@ -15,25 +29,36 @@ import (
 //
 // Default criteria for the from and to block are "latest".
 // Using "latest" as block number will return logs for mined blocks.
-// Using "pending" as block number returns logs for not yet mined (pending) blocks.
+// Using "pending" as block number is not supported.
 // In case logs are removed (chain reorg) previously returned logs are returned
 // again but with the removed property set to true.
 //
 // In case "fromBlock" > "toBlock" an error is returned.
-func (api *ethAPI) NewFilter(ctx context.Context, filter web3Types.FilterQuery) (rpc.ID, error) {
+func (api *ethAPI) NewFilter(ctx context.Context, fq web3Types.FilterQuery) (rpc.ID, error) {
 	w3c := GetEthClientFromContext(ctx)
-	// TODO: integrate golang sdk
-	_ = w3c
-	return "", errors.New("not supported yet")
+
+	fid, err := w3c.Filter.NewLogFilter(&fq)
+	if err != nil {
+		return "", err
+	}
+
+	cachekey := ethFilterQueryCacheKey(w3c.URL, *fid)
+	ethFilterQueryCache.Add(cachekey, &fq)
+
+	return *fid, nil
 }
 
 // NewBlockFilter creates a filter that fetches blocks that are imported into the chain.
 // It is part of the filter package since polling goes with eth_getFilterChanges.
 func (api *ethAPI) NewBlockFilter(ctx context.Context) (rpc.ID, error) {
 	w3c := GetEthClientFromContext(ctx)
-	// TODO: integrate golang sdk
-	_ = w3c
-	return "", errors.New("not supported yet")
+
+	fid, err := w3c.Filter.NewBlockFilter()
+	if err != nil {
+		return "", err
+	}
+
+	return *fid, nil
 }
 
 // NewPendingTransactionFilter creates a filter that fetches pending transaction hashes
@@ -43,17 +68,24 @@ func (api *ethAPI) NewBlockFilter(ctx context.Context) (rpc.ID, error) {
 // `eth_getFilterChanges` polling method that is also used for log filters.
 func (api *ethAPI) NewPendingTransactionFilter(ctx context.Context) (rpc.ID, error) {
 	w3c := GetEthClientFromContext(ctx)
-	// TODO: integrate golang sdk
-	_ = w3c
-	return "", errors.New("not supported yet")
+
+	fid, err := w3c.Filter.NewPendingTransactionFilter()
+	if err != nil {
+		return "", err
+	}
+
+	return *fid, nil
 }
 
 // UninstallFilter removes the filter with the given filter id.
-func (api *ethAPI) UninstallFilter(ctx context.Context, id rpc.ID) (bool, error) {
+func (api *ethAPI) UninstallFilter(ctx context.Context, fid rpc.ID) (bool, error) {
 	w3c := GetEthClientFromContext(ctx)
-	// TODO: integrate golang sdk
-	_ = w3c
-	return false, errors.New("not supported yet")
+
+	// remove from the LRU cache if existed
+	cachekey := ethFilterQueryCacheKey(w3c.URL, fid)
+	ethFilterQueryCache.Remove(cachekey)
+
+	return w3c.Filter.UninstallFilter(fid)
 }
 
 // GetFilterChanges returns the logs for the filter with the given id since
@@ -61,18 +93,59 @@ func (api *ethAPI) UninstallFilter(ctx context.Context, id rpc.ID) (bool, error)
 //
 // For pending transaction and block filters the result is []common.Hash.
 // (pending)Log filters return []Log.
-func (api *ethAPI) GetFilterChanges(ctx context.Context, id rpc.ID) (interface{}, error) {
+func (api *ethAPI) GetFilterChanges(ctx context.Context, fid rpc.ID) (interface{}, error) {
 	w3c := GetEthClientFromContext(ctx)
-	// TODO: integrate golang sdk
-	_ = w3c
-	return false, errors.New("not supported yet")
+
+	// refresh the LRU cache
+	cachekey := ethFilterQueryCacheKey(w3c.URL, fid)
+	_, ok := ethFilterQueryCache.Get(cachekey)
+
+	res, err := w3c.Filter.GetFilterChanges(fid)
+	if detectFilterNotFoundError(err) && ok {
+		// remove cache if filter not found on full node any more
+		ethFilterQueryCache.Remove(cachekey)
+	}
+
+	return res, err
 }
 
 // GetFilterLogs returns the logs for the filter with the given id.
 // If the filter could not be found an empty array of logs is returned.
-func (api *ethAPI) GetFilterLogs(ctx context.Context, id rpc.ID) ([]*types.Log, error) {
+func (api *ethAPI) GetFilterLogs(ctx context.Context, fid rpc.ID) ([]web3Types.Log, error) {
 	w3c := GetEthClientFromContext(ctx)
-	// TODO: integrate golang sdk
-	_ = w3c
-	return nil, errors.New("not supported yet")
+
+	cachekey := ethFilterQueryCacheKey(w3c.URL, fid)
+	v, ok := ethFilterQueryCache.Get(cachekey)
+	if !ok {
+		return ethEmptyLogs, errFilterNotFound
+	}
+
+	fq := v.(*web3Types.FilterQuery)
+	logs, delegated, err := api.getLogs(ctx, fq, true)
+
+	logger := api.filterLogger(fq).WithField("fnDelegated", delegated)
+
+	if err != nil {
+		logger.WithError(err).Debug("Failed to handle `eth_getFilterLogs` RPC request")
+		return logs, err
+	}
+
+	logger.Debug("`eth_getFilterLogs` RPC request handled")
+	return logs, nil
+}
+
+// ethFilterQueryCacheKey assembles filter query cache key for specified filter ID and fullnode
+func ethFilterQueryCacheKey(nodeUrl string, fid rpc.ID) string {
+	fn := rpcutil.Url2NodeName(string(nodeUrl))
+	return fmt.Sprintf("%s/%s", fn, fid)
+}
+
+// detectFilterNotFoundError detects `filter not found` error according to error content
+func detectFilterNotFoundError(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	errStr := strings.ToLower(err.Error())
+	return strings.Contains(errStr, "filter not found")
 }
