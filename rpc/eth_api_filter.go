ec61377a2844a3783473b0ca0d9859758e45534b
wanliqun <wanliqunn@163.com>
2023-03-21 11:00:26 +0800

    Optimize filter API alert warning (#111)

--- rpc/eth_api_filter.go
+++ rpc/eth_api_filter.go
@@ -2,23 +2,21 @@ package rpc
 
 import (
 	"context"
-	"errors"
 
 	"github.com/Conflux-Chain/confura/util/metrics"
 	"github.com/openweb3/go-rpc-provider"
-	"github.com/openweb3/go-rpc-provider/utils"
 	web3Types "github.com/openweb3/web3go/types"
-	"github.com/sirupsen/logrus"
+	"github.com/pkg/errors"
 )
 
 const (
 	rpcMethodEthNewFilter = "eth_newFilter"
 )
 
-var (
-	// uniform virtual filter connection error
-	errVirtualFilterConnError = errors.New("virtual filter connection error")
-)
+// uniform virtual filter proxy error
+func errVirtualFilterProxyErrorOrNil(err error) error {
+	return errors.WithMessage(err, "virtual filter proxy error")
+}
 
 // NewFilter creates a new filter and returns the filter id. It can be
 // used to retrieve logs when the state changes. This method cannot be
@@ -36,25 +34,16 @@ func (api *ethAPI) NewFilter(ctx context.Context, fq web3Types.FilterQuery) (*rp
 	metrics.UpdateEthRpcLogFilter(rpcMethodEthNewFilter, w3c.Eth, &fq)
 
 	fid, err := api.VirtualFilterClient.NewFilter(w3c.URL, &fq)
-	if err != nil && !utils.IsRPCJSONError(err) {
-		logrus.WithError(err).Error("Failed to connect virtual filter for new log filter")
-		return fid, errVirtualFilterConnError
-	}
-
-	return fid, err
+	return fid, errVirtualFilterProxyErrorOrNil(err)
 }
 
 // NewBlockFilter creates a filter that fetches blocks that are imported into the chain.
 // It is part of the filter package since polling goes with eth_getFilterChanges.
 func (api *ethAPI) NewBlockFilter(ctx context.Context) (*rpc.ID, error) {
 	w3c := GetEthClientFromContext(ctx)
-	fid, err := api.VirtualFilterClient.NewBlockFilter(w3c.URL)
-	if err != nil && !utils.IsRPCJSONError(err) {
-		logrus.WithError(err).Error("Failed to connect virtual filter for new block filter")
-		return fid, errVirtualFilterConnError
-	}
 
-	return fid, err
+	fid, err := api.VirtualFilterClient.NewBlockFilter(w3c.URL)
+	return fid, errVirtualFilterProxyErrorOrNil(err)
 }
 
 // NewPendingTransactionFilter creates a filter that fetches pending transaction hashes
@@ -64,24 +53,15 @@ func (api *ethAPI) NewBlockFilter(ctx context.Context) (*rpc.ID, error) {
 // `eth_getFilterChanges` polling method that is also used for log filters.
 func (api *ethAPI) NewPendingTransactionFilter(ctx context.Context) (*rpc.ID, error) {
 	w3c := GetEthClientFromContext(ctx)
-	fid, err := api.VirtualFilterClient.NewPendingTransactionFilter(w3c.URL)
-	if err != nil && !utils.IsRPCJSONError(err) {
-		logrus.WithError(err).Error("Failed to connect virtual filter for new pending txn filter")
-		return fid, errVirtualFilterConnError
-	}
 
-	return fid, err
+	fid, err := api.VirtualFilterClient.NewPendingTransactionFilter(w3c.URL)
+	return fid, errVirtualFilterProxyErrorOrNil(err)
 }
 
 // UninstallFilter removes the filter with the given filter id.
 func (api *ethAPI) UninstallFilter(ctx context.Context, fid rpc.ID) (bool, error) {
 	ok, err := api.VirtualFilterClient.UninstallFilter(fid)
-	if err != nil && !utils.IsRPCJSONError(err) {
-		logrus.WithError(err).Error("Failed to connect virtual filter to uninstall filter")
-		return ok, errVirtualFilterConnError
-	}
-
-	return ok, err
+	return ok, errVirtualFilterProxyErrorOrNil(err)
 }
 
 // GetFilterChanges returns the logs for the filter with the given id since
@@ -91,22 +71,12 @@ func (api *ethAPI) UninstallFilter(ctx context.Context, fid rpc.ID) (bool, error
 // (pending)Log filters return []Log.
 func (api *ethAPI) GetFilterChanges(ctx context.Context, fid rpc.ID) (interface{}, error) {
 	res, err := api.VirtualFilterClient.GetFilterChanges(fid)
-	if err != nil && !utils.IsRPCJSONError(err) {
-		logrus.WithError(err).Error("Failed to connect virtual filter to get filter changes")
-		return res, errVirtualFilterConnError
-	}
-
-	return res, err
+	return res, errVirtualFilterProxyErrorOrNil(err)
 }
 
 // GetFilterLogs returns the logs for the filter with the given id.
 // If the filter could not be found an empty array of logs is returned.
 func (api *ethAPI) GetFilterLogs(ctx context.Context, fid rpc.ID) ([]web3Types.Log, error) {
 	logs, err := api.VirtualFilterClient.GetFilterLogs(fid)
-	if err != nil && !utils.IsRPCJSONError(err) {
-		logrus.WithError(err).Error("Failed to connect virtual filter to get filter logs")
-		return logs, errVirtualFilterConnError
-	}
-
-	return logs, err
+	return logs, errVirtualFilterProxyErrorOrNil(err)
 }
