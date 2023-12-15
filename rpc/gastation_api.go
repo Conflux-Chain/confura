ec1bd5613ab715119130fc6e2e2dcd0c32d3fb08
Conflux <wanliqunn@163.com>
2023-12-15 16:32:16 +0800

    Use oracle gas price for gastation_api

--- rpc/gastation_api.go
+++ rpc/gastation_api.go
@@ -17,5 +17,19 @@ func newGasStationAPI(handler *handler.GasStationHandler) *gasStationAPI {
 }
 
 func (api *gasStationAPI) Price(ctx context.Context) (*types.GasStationPrice, error) {
-	return api.handler.GetPrice()
+	//return api.handler.GetPrice()
+
+	// Use oracle gas price from the blockchain.
+	cfx := GetCfxClientFromContext(ctx)
+	price, err := cfx.GetGasPrice()
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.GasStationPrice{
+		Fast:    price,
+		Fastest: price,
+		SafeLow: price,
+		Average: price,
+	}, nil
 }
